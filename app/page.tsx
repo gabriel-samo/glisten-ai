e9302f4f9decbce2e0274f3fe082de5bcabcf8f8
Gabriel Samoylov <gsamo122@gmail.com>
2024-10-08 22:48:39 +0300

    configering prettier for tailwind auto sort classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className="container">
+      <h1 className="text-center text-4xl font-bold">Hello World</h1>
     </main>
   );
 }
